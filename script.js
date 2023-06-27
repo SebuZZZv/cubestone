6b38012d698a78327efe3c5400cd41edfa99ee0a
Sebastian <sebastian.bak@o2.pl>
2023-06-27 18:37:10 +0200

    mobile bugs fixed #2

--- script.js
+++ script.js
@@ -21,4 +21,10 @@ exit.addEventListener("click", () => {
      }
   });
 
-  
+  function callback(entries) {
+    if (entries[0].isIntersecting) {
+      window.scrollTo({top: Number(6200 - window.innerHeight), left: 0, behavior: 'smooth'});
+    }
+  }
+    const observer = new IntersectionObserver(callback);
+    observer.observe(document.querySelector('.box-of-regu'));
